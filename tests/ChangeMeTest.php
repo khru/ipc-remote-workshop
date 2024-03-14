99f40a9477b506f78423c86071da23629081c84c
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2024-03-14 16:46:25 +0100

    refactor¨: remove duplication on the test creating the game

--- tests/ChangeMeTest.php
+++ tests/ChangeMeTest.php
@@ -9,27 +9,28 @@ use PHPUnit\Framework\TestCase;
 
 final class ChangeMeTest extends TestCase
 {
+    private RockPaperScissorsGame $game;
+
+    public function setUp(): void
+    {
+        $this->game = new RockPaperScissorsGame();
+    }
+
     /** @test */
     public function given_player_1_choose_rock_when_the_player_2_chooses_scissors_then_player_1_should_win(): void
     {
-        $game = new RockPaperScissorsGame();
-
-        self::assertSame("p1", $game->play("r","s"));
+        self::assertSame("p1", $this->game->play("r","s"));
     }
 
     /** @test */
     public function given_player_1_choose_scissors_when_the_player_2_chooses_rock_then_player_2_should_win(): void
     {
-        $game = new RockPaperScissorsGame();
-
-        self::assertSame("p2", $game->play("s","r"));
+        self::assertSame("p2", $this->game->play("s","r"));
     }
 
     /** @test */
     public function given_player_1_choose_paper_when_the_player_2_chooses_rock_then_player_1_should_win(): void
     {
-        $game = new RockPaperScissorsGame();
-
-        self::assertSame("p1", $game->play("p","r"));
+        self::assertSame("p1", $this->game->play("p","r"));
     }
 }
