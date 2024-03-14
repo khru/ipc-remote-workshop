d8cd8d5f5e09050dcd2a2aa83817e9168766047d
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2024-03-14 16:43:37 +0100

    green: given_player_1_choose_paper_when_the_player_2_chooses_rock_then_player_1_should_win

--- src/RockPaperScissorsGame.php
+++ src/RockPaperScissorsGame.php
@@ -8,9 +8,14 @@ final class RockPaperScissorsGame
 {
     public function play(string $playerOneMove, string $playerTwoMove): string
     {
+        if ($playerOneMove === 'p') {
+            return "p1";
+        }
+
         if ($playerOneMove === 'r'){
             return "p1";
         }
+
         return "p2";
     }
 }
